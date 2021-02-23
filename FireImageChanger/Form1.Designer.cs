acafcb448928d37dfb28baa672aeb432030d5cfe
Ogura Deko <fanta51@live.jp>
2021-02-23 23:28:36 +0900

    v1

---
+++ FireImageChanger/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Firefox_NewTab_Background_Image_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(59, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(284, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Profile:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Path:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(317, 19);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "* Firefox not installed or profile cannot found !! *";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Image:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(58, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(284, 19);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "..";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(17, 143);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(172, 89);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enable on";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(7, 62);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(134, 16);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "about:privatebrowsing";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(7, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(90, 16);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "about:newtab";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(7, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 16);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "about:home";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox5);
+            this.groupBox2.Controls.Add(this.checkBox4);
+            this.groupBox2.Location = new System.Drawing.Point(249, 143);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(126, 89);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Enabled = false;
+            this.checkBox5.Location = new System.Drawing.Point(7, 41);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(106, 16);
+            this.checkBox5.TabIndex = 1;
+            this.checkBox5.TabStop = false;
+            this.checkBox5.Text = "userContent.css";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Enabled = false;
+            this.checkBox4.Location = new System.Drawing.Point(7, 18);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(87, 16);
+            this.checkBox4.TabIndex = 0;
+            this.checkBox4.TabStop = false;
+            this.checkBox4.Text = "image folder";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(363, 26);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Create image folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(363, 26);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Apply background image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 302);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(363, 26);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "not working?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(348, 14);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(27, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "?";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 340);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Fire Image Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ FireImageChanger/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Firefox_NewTab_Background_Image_Changer
+{
+    public partial class Form1 : Form
+    {
+        string appname = "Firefox NewTab Background Image Changer";
+        string[] Dirct = new string[99];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Wakeup load
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            path = path + "\\Mozilla\\Firefox\\Profiles";
+
+            //フォルダ内のすべてのフォルダを取得
+            string[] allDirectories = Directory.GetDirectories(path);
+            int i = 0;
+            foreach (string str in allDirectories)
+            {
+                string s = str.Replace(path + "\\", "");
+                Dirct[i] += s;
+                i++;
+            }
+            comboBox1.DataSource = Dirct;
+
+            if (i >= 1)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //select image button clicked
+            string s_image;
+            openFileDialog1.Title = "Select background image";
+            openFileDialog1.Filter = "Image (*.png;*.jpg)|*.png;*.jpg";
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                s_image = openFileDialog1.FileName;
+            }
+            else
+            {
+                return;
+            }
+            textBox2.Text = s_image;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //create chrome folder
+            if (!Directory.Exists(textBox1.Text))
+            {
+                //folder not found
+                MessageBox.Show("Selected profile cannot found!", appname, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (Directory.Exists(textBox1.Text + "\\chrome\\img"))
+            {
+                //chrome already created
+                MessageBox.Show("image folder already created!", appname, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                //make chrome folder
+                if (!Directory.Exists(textBox1.Text + "\\chrome"))
+                {
+                    Directory.CreateDirectory(textBox1.Text + "\\chrome");
+                }
+                Directory.CreateDirectory(textBox1.Text + "\\chrome\\img");
+                MessageBox.Show("image folder created successfully!", appname, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                checkBox4.Checked = true;
+            }
+            return;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //apply background image
+            if (textBox2.Text != "")
+            {
+                if (!File.Exists(textBox2.Text))
+                {
+                    //image not found
+                    MessageBox.Show("Selected image cannot found!", appname, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select image!", appname, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (!Directory.Exists(textBox1.Text + "\\chrome\\img"))
+            {
+                //image folder not found
+                MessageBox.Show("image folder not found!\n[Create image folder] first.", appname, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string csstext = "";
+            string target = "url(about:none)";
+            string imgpath = "";
+
+            if (checkBox1.Checked)
+            {
+                target += ", url(about:home)";
+            }
+
+            if (checkBox2.Checked)
+            {
+                target += ", url(about:newtab)";
+            }
+
+            if (checkBox3.Checked)
+            {
+                target += ", url(about:privatebrowsing)";
+            }
+
+            string imgname = System.IO.Path.GetExtension(textBox2.Text);
+
+            csstext = "@-moz-document " + target + " { body{ content: ''; z-index: -1; position: fixed; top: 0; left: 0; background: #f9a no-repeat url(./img/bg" + imgname + ") center; background-size: cover; width: 100vw; height: 100vh;} }";
+
+            if (File.Exists(textBox1.Text + "\\chrome\\img\\bg" + imgname))
+            {
+                //already created
+                DialogResult dr = MessageBox.Show("Overwrite background image?", appname, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    FileSystem.CopyFile(textBox2.Text, textBox1.Text + "\\chrome\\img\\bg" + imgname, UIOption.OnlyErrorDialogs);
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                //not created
+                FileSystem.CopyFile(textBox2.Text, textBox1.Text + "\\chrome\\img\\bg" + imgname, UIOption.OnlyErrorDialogs);
+            }
+
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");
+            using (StreamWriter writer = new StreamWriter(textBox1.Text + "\\chrome\\userContent.css", false, enc))
+            {
+                writer.WriteLine(csstext);
+            }
+
+            MessageBox.Show("background image apply successfully!", appname, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //not working
+            MessageBox.Show("1. Open Firefox\n2. Enter 'about:config' on URL bar\n3. Type 'toolkit.legacyUserProfileCustomizations.stylesheets' on search box\n4. Change 'False' to 'True'\n5. Restart Firefox", appname, MessageBoxButtons.OK);
+            return;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //profile change event
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            path = path + "\\Mozilla\\Firefox\\Profiles";
+            int i = comboBox1.SelectedIndex;
+            textBox1.Text = path + "\\" + Dirct[i];
+
+            if (Directory.Exists(textBox1.Text + "\\chrome"))
+            {
+                checkBox4.Checked = true;
+            }
+            else
+            {
+                checkBox4.Checked = false;
+            }
+
+            if (File.Exists(textBox1.Text + "\\chrome\\userContent.css"))
+            {
+                checkBox5.Checked = true;
+            }
+            else
+            {
+                checkBox5.Checked = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //what is profile?
+            MessageBox.Show("Firefox stores user settings and other information in each profile. Here, select the profile you want to customize the background for.\nYou can see the profile in use at [about:profiles].\n\nFor example, if the root directory string ends with [thisisexample.default-esr], that is the profile in use.", appname, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+        }
+    }
+}
